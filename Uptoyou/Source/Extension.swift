606b9ed3e392ded9e982bf321c75c3c62061895a
tihtra <artdevil.t.t@gmail.com>
2020-11-06 14:03:30 +0700

    Start Library

---
+++ Uptoyou/Source/Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Extension.swift
+//  Uptoyou
+//
+//  Created by Devil on 6/11/2563 BE.
+//
+
+import UIKit
+
+public extension UILabel {
+    public func underLine(){
+        self.backgroundColor = UIColor.orange
+    }
+}
