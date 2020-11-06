6267c07d62f5cc5bd7200471e1071be96fd916a9
tihtra <artdevil.t.t@gmail.com>
2020-11-06 13:52:32 +0700

    Initial Commit

---
+++ Uptoyou/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Uptoyou
+//
+//  Created by Devil on 6/11/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
